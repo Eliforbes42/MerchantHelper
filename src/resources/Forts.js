be0b6036c1de3261bfd6674d1eaa292e33c0ff20
Eli Forbes <eliforbes42@gmail.com>
2020-09-30 09:57:34 -0700

    Updated package.json and resources

--- src/resources/Forts.js
+++ src/resources/Forts.js
@@ -1,10 +1,14 @@
 import Fort from './Fort';
 
 let Forts = [
-        new Fort('K',9, "Hidden Spring Keep"), new Fort('C',7, "Keel Haul Fort"),
-        new Fort('O',6,"Kraken Watchtower"), new Fort('J',21,"Lost Gold Fort"),
-        new Fort('P',17,"Old Boot Fort"), new Fort('U',5,"Shark Fin Camp"),
-        new Fort('E',17,"Sailor's Knot Stronghold"), new Fort('U',11,"Skull Keep"),
+        new Fort('K',9, "Hidden Spring Keep"), 
+        new Fort('C',7, "Keel Haul Fort"),
+        new Fort('O',6,"Kraken Watchtower"), 
+        new Fort('J',21,"Lost Gold Fort"),
+        new Fort('P',17,"Old Boot Fort"), 
+        new Fort('U',5,"Shark Fin Camp"),
+        new Fort('E',17,"Sailor's Knot Stronghold"), 
+        new Fort('U',11,"Skull Keep"),
         new Fort('S',22,"The Crow's Nest Fortress")
 ];
 
